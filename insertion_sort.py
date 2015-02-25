f0b5eb07637e290a15c227ef2075b37c694180a6
Kabir Kang <kangkabir@gmail.com>
2015-02-25 11:40:04 -0800

    Completed insertion

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,5 +1,13 @@
-def sorting_method(list_of_nums):
-    break
+# Insertion sort implementation in Python
+def sorting_method(nums_list):
+    for j in range(1, len(nums_list)):
+        key = nums_list[j]
+        i = j-1
+        while i>=0 and nums_list[i] > key:
+            nums_list[i+1] = nums_list[i]
+            i-=1
+        nums_list[i+1] = key
+    return nums_list
 
 def main():
     nums = [5,2,3,6,1,7,8,9,4]
